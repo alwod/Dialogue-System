b71cc1034b495f346081cb94c4807c0498c5a94f
Mikha <mikharohrs@gmail.com>
2023-08-06 13:24:19 +0100

    Added placeholder brightness option

--- Dialogue Testing Project/Scripts/options_menu.gd
+++ Dialogue Testing Project/Scripts/options_menu.gd
@@ -24,6 +24,7 @@ func pressed():
 # Changes the camera sensitivity in the project settings.
 func _on_sensitivity_slider_value_changed(value):
 	ProjectSettings.set_setting("player/camera_sensitivity", value)
+	#$TabContainer/Gameplay/MarginContainer/GridContainer/HBoxContainer/DisplaySensitivity.text = value as String
 
 # This is a function as not all UI elements that need to play this sound when clicked, are buttons.
 func play_button_sound():
