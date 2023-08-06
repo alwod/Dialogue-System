377211bcd3f23795ac4cd873c899d3883021c3dc
Mikha <mikharohrs@gmail.com>
2023-08-06 12:49:29 +0100

    Updated comments.
    Made the comments in the options menu script more clear and added a placeholder FPS display button.

--- Dialogue Testing Project/Scripts/options_menu.gd
+++ Dialogue Testing Project/Scripts/options_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-# Called when the display mode is changed.
+# Changes the display mode of the window. - BORDERLESS NOT COMPLETE.
 func _on_display_mode_options_button_item_selected(index):
 	match index:
 		0:
@@ -10,19 +10,25 @@ func _on_display_mode_options_button_item_selected(index):
 		2:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
-# Called when vsync button is pressed.
+# Enables / disables VSync.
 func _on_vsync_check_button_toggled(button_pressed):
 	if (button_pressed):
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
 	else:
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
 
-# Called when any button is pressed to play a sound.
+# Called when a button is pressed. Plays the button-pressing sound.
 func pressed():
 	play_button_sound()
 
+# Changes the camera sensitivity in the project settings.
 func _on_sensitivity_slider_value_changed(value):
 	ProjectSettings.set_setting("player/camera_sensitivity", value)
 
+# This is a function as not all UI elements that need to play this sound when clicked, are buttons.
 func play_button_sound():
 	$ButtonPressSoundPlayer.play()
+
+# Displays the FPS - NOT COMPLETE.
+func _on_display_fps_button_toggled(button_pressed):
+	pass # Replace with function body.
